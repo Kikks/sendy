cc303ff2baf7c256b776518581455d3fd36fc94a
Raymond Ativie <RaymondAtivie@gmail.com>
2019-10-11 18:06:35 +0100

    contact

--- src/views/home/Contacts.vue
+++ src/views/home/Contacts.vue
@@ -1,22 +1,41 @@
 <template>
     <div class="activity">
         <div class="recent-activity">
-            <h2 class="mt-3">All Contacts</h2>
+            <div class="row align-items-center justify-content-space-between my-2">
+                <div class="col">
+                    <h2>All Contacts</h2>
+                </div>
+                <div class="col text-right">
+                    <button class="addBtn" @click="addNewContact()">+ Add New</button>
+                </div>
+            </div>
 
             
         </div>
     </div>
 </template>
 
+<script>
+export default {
+    methods: {
+        addNewContact(){
+            // this.$router.push({name: 'new-contact'});
+        }
+    }
+}
+</script>
+
 <style lang="scss" scoped>
     .activity{
         min-height: 100vh;
+        .addBtn{
+            color: $primary;
+            font-weight: normal;
+        }
     }
     .recent-activity{
         padding: 20px;
         background-color: white;
-        height: 800px;
-        border-radius: 20px 20px 0px 0px;  
 
         h2{
             font-size: 17px;
