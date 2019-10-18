ef7bbfc7ec50b09b6b72ec8ad4a0c8deaf0c81c7
Daniel <adedejiayokunledaniel@gmail.com>
2019-10-18 10:27:04 +0100

    morning push

--- src/views/airtime/SendAirtime.vue
+++ src/views/airtime/SendAirtime.vue
@@ -3,29 +3,37 @@
         <GreyNavbar title="Send Airtime" />
 
         <div class="send-airtime">
-            <h3><b>Select Categories</b></h3>
+            <h3>
+                <b>Select Categories</b>
+            </h3>
 
             <div class="switcher mt-3">
-                <div @click="tab=true" class="switch" :class="tab && 'active'">
-                    New Recipient
-                </div>
-                <div @click="tab=false" class="switch" :class="!tab && 'active'">
-                    Saved Recipients
-                </div>
-            </div> 
+                <div @click="tab=true" class="switch" :class="tab && 'active'">New Recipient</div>
+                <div @click="tab=false" class="switch" :class="!tab && 'active'">Saved Recipients</div>
+            </div>
             <div v-if="tab">
                 <tl-input class="mt-5" placeholder="Name" v-model="name" />
                 <tl-input class="mt-5" placeholder="Phone" type="tel" v-model="phoneNumber" />
-                <tl-input class="mt-5" placeholder="Airtime Amount" type="number" v-model="airtimeAmount" />
+                <tl-input
+                    class="mt-5"
+                    placeholder="Airtime Amount"
+                    type="number"
+                    v-model="airtimeAmount"
+                />
             </div>
             <div v-else>
-                <tl-input class="mt-5" v-model="searchTerm" placeholder="Search Recipient" icon="magnify"/>
+                <tl-input
+                    class="mt-5"
+                    v-model="searchTerm"
+                    placeholder="Search Recipient"
+                    icon="magnify"
+                />
                 <div class="search" v-if="searchTerm.length > 0">
                     <div class="searchbox px-3 pb-3">
                         <div class="searchitem pt-3" v-for="(n,i) in 3" :key="i">
                             <div class="row">
                                 <div class="col-6">Nathan</div>
-                                <div class="col-6 text-right" >NGN500</div>
+                                <div class="col-6 text-right">NGN500</div>
                             </div>
                             <div class="row searchrow">
                                 <div class="col-6">+234 801 123 4567</div>
@@ -34,35 +42,60 @@
                         </div>
                     </div>
                 </div>
-                <tl-input class="mt-5" placeholder="Airtime Amount" type="number" v-model="airtimeAmount" />
+                <tl-input
+                    class="mt-5"
+                    placeholder="Airtime Amount"
+                    type="number"
+                    v-model="airtimeAmount"
+                />
             </div>
 
-            <button class="btn mt-5" @click="transfer()">Transfer</button>
+            <button class="btn mt-5" :disabled="canSubmit || isLoading" @click="transfer()">
+                <icon name="loading" spin size="0.9" class="mr-1" v-if="isLoading" />Transfer
+            </button>
         </div>
-
     </div>
 </template>
 <script>
-import axios from 'axios';
-import Helpers from '../../utils/Helpers';
+import axios from "axios";
+import Helpers from "../../utils/Helpers";
 export default {
-    data(){
-        return{
+    data() {
+        return {
             tab: true,
             // search: true,
-            searchTerm: '',
+            searchTerm: "",
             name: "",
             phoneNumber: "",
-            airtimeAmount: ""
+            airtimeAmount: "+2348060917025",
+            isLoading: false
+        };
+    },
+    computed: {
+        canSubmit() {
+            if (
+                (this.tab && this.name.length < 1) ||
+                (this.tab && this.phoneNumber.length < 1)
+            ) {
+                return true;
+            }
 
+            if (this.airtimeAmount.length < 1) {
+                return true;
+            }
+
+            return false;
         }
     },
     methods: {
-        transfer(){
+        transfer() {
+            if (this.isLoading) return;
 
             const url = `${process.env.VUE_APP_SENDY_SVC_URL}/sendy/airtime`;
-
-            axios.get(`${process.env.VUE_APP_SENDY_SVC_URL}/sendy/user`).then(response => console.log(response));
+            this.isLoading = true;
+            axios
+                .get(`${process.env.VUE_APP_SENDY_SVC_URL}/sendy/user`)
+                .then(response => console.log(response));
             const data = {
                 value: Number(this.airtimeAmount),
                 category: this.tab ? "newRecipient" : "savedRecipient",
@@ -73,74 +106,76 @@ export default {
             axios
                 .post(url, data)
                 .then(response => {
-                    console.log(response);
+                    this.isLoading = false;
+                    this.$toasted.show(response.data.message);
+                    this.$router.push({ name: "home" });
                 })
                 .catch(error => {
                     Helpers.errorResponse(error, response => {
-                        console.log(response);
+                        this.isLoading = false;
+                        this.$toasted.show(response);
                     });
                 });
-
         }
-    },
-}
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-    .send-airtime{
-        margin-top: 55px;
-        padding: 20px;
+.send-airtime {
+    margin-top: 55px;
+    padding: 20px;
 
-         .switcher{
-            border: 1px solid $primary;
-            border-radius: 5px;
-            text-align: center;
-            display: flex;
+    .switcher {
+        border: 1px solid $primary;
+        border-radius: 5px;
+        text-align: center;
+        display: flex;
 
-            .switch{
-                flex: 1;
-                padding: 4px 0;
-                color: $primary;
-                font-size: 16px;
-            }
-            .active{
-                background-color: $primary;
-                color: #FFFFFF; 
-                //    border:1px solid blue;
-                    // border-radius:5px;
-            }
+        .switch {
+            flex: 1;
+            padding: 4px 0;
+            color: $primary;
+            font-size: 16px;
         }
-        .fixedbtn{
-            position: fixed;
-            top:680px;
-            left:3.5px;
-            width:98%;
-        }
-        .search{
-            position:absolute;
-            width:90%;
-            // max-width: calc($full-width - 10px);
-            max-width: $full-width;
-        }
-        .searchbox{
-            width: 100%;
-            max-width: $full-width;
-            height:300px;
-            background: #FFFFFF;
-            box-shadow: 0px 20px 50px #E9EDEE;
-            border-radius: 0px 0px 10px 10px;
-
-            .searchitem{
-                border-bottom: 1px solid#E6EDFF;;
-                &:last-child{
-                    border-bottom: none;
-                }
-            }
-
-            .searchrow{
-                color:#D2D5DB;
-                font-size:14px;
-            }
+        .active {
+            background-color: $primary;
+            color: #ffffff;
+            //    border:1px solid blue;
+            // border-radius:5px;
         }
     }
+    .fixedbtn {
+        position: fixed;
+        top: 680px;
+        left: 3.5px;
+        width: 98%;
+    }
+    .search {
+        position: absolute;
+        width: 90%;
+        // max-width: calc($full-width - 10px);
+        max-width: $full-width;
+    }
+    .searchbox {
+        width: 100%;
+        max-width: $full-width;
+        height: 300px;
+        background: #ffffff;
+        box-shadow: 0px 20px 50px #e9edee;
+        border-radius: 0px 0px 10px 10px;
+
+        .searchitem {
+            border-bottom: 1px solid#E6EDFF;
+            &:last-child {
+                border-bottom: none;
+            }
+        }
+
+        .searchrow {
+            color: #d2d5db;
+            font-size: 14px;
+        }
+    }
+}
 </style>
--- src/views/home/Topup.vue
+++ src/views/home/Topup.vue
@@ -3,82 +3,89 @@
         <div class="recent-activity">
             <h2 class="mt-3">Fund Sendy Wallet</h2>
 
-            <tl-input class="amount" placeholder="Enter Amount" />
+            <tl-input class="amount" placeholder="Enter Amount" v-model="airtimeAmount" />
 
-            <button class="btn mt-5" @click="pay">Continue</button>
+            <button class="btn mt-5" @click="pay" :disabled="airtimeAmount.length < 2">
+                <icon name="loading" spin size="0.9" class="mr-1" v-if="isLoading" />Continue
+            </button>
 
-            <p class="mt-5 text-center pt-5">
-                Having your wallet funded keeps your contacts credited recurrently
-            </p>
+            <p
+                class="mt-5 text-center pt-5"
+            >Having your wallet funded keeps your contacts credited recurrently</p>
         </div>
     </div>
 </template>
 <script>
-import axios from 'axios';
-import Helpers from '../../utils/Helpers';
+import axios from "axios";
+import Helpers from "../../utils/Helpers";
 export default {
-    data(){
+    data() {
         return {
-
+            airtimeAmount: "",
+            isLoading: false
         };
     },
+    computed: {
+        user() {
+            return this.$store.getters.getUser;
+        },
+        canSubmit() {
+            if (this.airtimeAmount) return false;
+        }
+    },
     methods: {
-        pay(){
-            let x = PaystackPop.setup({
-                key: 'pk_test_38a312e4b92df2c8768937d0aef988143689589b',
-                email: 'danieel@gmail.com', //danieel@gmail.com adedejiayokunledaniel@gmail.com
-                amount: 100000+(65),
+        pay() {
+            let handler = PaystackPop.setup({
+                key: `${process.env.VUE_APP_PAYSTACK_TEST_KEY}`,
+                email: this.user.email, //danieel@gmail.com adedejiayokunledaniel@gmail.com
+                amount: Number(this.airtimeAmount) * 100,
                 currency: "NGN",
                 callback: response => {
-                    console.log(response);
-                    this.refPay(response);
+                    this.referencePayment(response);
                 },
-                onClose: () => console.log('closed')
+                onClose: () => console.log("closed")
             });
-            x.openIframe();
+            handler.openIframe();
         },
-        refPay(response){
-            console.log('start ref pay');
+        referencePayment(response) {
+            if (this.isLoading) return;
+
+            this.isLoading = true;
             const url = `${process.env.VUE_APP_SENDY_SVC_URL}/sendy/payment`;
             axios
                 .post(url, {
                     reference: response.reference
                 })
                 .then(response => {
-                    console.log(response);
-                    this.we();
+                    this.isLoading = false;
+                    this.$toasted.show(response.data.message);
+                    this.$router.push({ name: "home" });
                 })
                 .catch(error => {
                     Helpers.errorResponse(error, response => {
-                        console.log(error);
-                    })
+                        this.isLoading = false;
+                        this.$toasted.show(response);
+                    });
                 });
-        },
-        we(){
-            const url = `${process.env.VUE_APP_SENDY_SVC_URL}/sendy/transaction`;
-            axios.get(url)
-            .then(response => {
-                console.log(response);
-            });
         }
     }
-}
+};
 </script>
 <style lang="scss" scoped>
-    .activity{
-        min-height: 100vh;
+.activity {
+    min-height: 100vh;
+}
+.recent-activity {
+    padding: 20px;
+    background-color: white;
+    height: 800px;
+    border-radius: 20px 20px 0px 0px;
+    .amount {
+        margin-bottom: 80px;
     }
-    .recent-activity{
-        padding: 20px;
-        background-color: white;
-        height: 800px;
-        border-radius: 20px 20px 0px 0px;  
-        .amount{
-            margin-bottom:80px
-        }
-        h2{
-            font-size: 17px;
-            margin-bottom:120px;
-        }
+    h2 {
+        font-size: 17px;
+        margin-bottom: 120px;
     }
+}
 </style>
